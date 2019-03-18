9948cd9c8f9af357c4da8bca795714b5a2cccb91
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-18 18:56:51 -0500

    build error

--- Holiday/Program.cs
+++ Holiday/Program.cs
@@ -6,9 +6,44 @@ namespace Holiday
     {
         public static void Main()
         {
+            Console.WriteLine("Enter the date you will be leaving:");
+            DateTime leaveDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many days will you be gone?");
+            int daysGone = int.Parse( Console.ReadLine());
+
+            //string returnWeekDay = ReturnDay.WriteLine(weekday);
+
             Console.ReadLine();
         }
+        //TimeSpan elapsed = now.Subtract(leaveDate);
+        // double days = elapsed.TotalDays;
+        //Console.WriteLine("{0} was {1} days ago." , leaveDate, daysGone.ToString("0"));
+        
+        public static string DayReturning(int daysGone, DateTime leaveDate)
+        {
+   
+           
+            DateTime returnDate = leaveDate.AddDays(daysGone);
 
-        // TODO: Create a method that takes the day you are leaving on vacation and how many days you will be gone and return the name of the day of the week you will return.
+            //DateTime weekdayReturn;
+
+            return returnDate.DayOfWeek.ToString();
+        }
+            
+            
+        
+
+
+
+
     }
 }
+
+
+
+           
+
+
+              
+                
